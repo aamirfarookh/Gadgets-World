64d6b97d08eb49ba2455ed83952c9b9ad63657c9
Avinaw Kumar <0811kumaravinaw@gmail.com>
2023-02-23 22:52:46 +0530

    Signup Login page complete

--- scripts/userSignup.js
+++ scripts/userSignup.js
@@ -7,33 +7,53 @@ document.querySelector("form").addEventListener("submit",signup);
 
 function signup(event){
     event.preventDefault();
-    let name = document.querySelector("#name").value;
-    let email = document.querySelector("#email").value;
-    let pass = document.querySelector("#pass").value;
     let username = document.querySelector("#username").value;
+    let first_name = document.querySelector("#firstname").value;
+    let last_name = document.querySelector("#lastname").value;
+    let email = document.querySelector("#email").value;
+    let phone_number = document.querySelector("#phonenumber").value;
+    let address = document.querySelector("#address").value;
+    let pass = document.querySelector("#pass").value;
+    
     if(username === ""){
         let wrong = document.querySelector("#username");
         wrong.style.borderBottom ="1px solid red";
     }
-    if(name === ""){
-        let wrong = document.querySelector("#name");
+    if(first_name === ""){
+        let wrong = document.querySelector("#firstname");
+        wrong.style.borderBottom ="1px solid red";
+    }
+    if(last_name === ""){
+        let wrong = document.querySelector("#lastname");
         wrong.style.borderBottom ="1px solid red";
     }
     if(email === ""){
         let wrong = document.querySelector("#email");
         wrong.style.borderBottom ="1px solid red";
     }
+    if(phone_number === ""){
+        let wrong = document.querySelector("#phonenumber");
+        wrong.style.borderBottom ="1px solid red";
+    }
+    if(address === ""){
+        let wrong = document.querySelector("#address");
+        wrong.style.borderBottom ="1px solid red";
+    }
     if(pass === ""){
         let wrong = document.querySelector("#pass");
         wrong.style.borderBottom="thin solid red";
     }
-    if(email!="" && pass != "" && name != "" && username != ""){
+    if(username!="" && first_name!="" && last_name!="" && email!="" && phone_number!="" && address!="" && pass != "" ){
 
         let obj= {
             username: username,
-            "full-name": name,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            phone_number: phone_number,
+            address: address,
             password: pass,
-            email: email,  
+             
         }
         let signupReq = fetch("http://localhost:8080/users",{
             method : "POST",
@@ -46,19 +66,11 @@ function signup(event){
         }).then((data) =>{
             console.log(data);
             alert("You can Sign in now");
-            // window.location.href = "userLogin.html";
+            window.location.href = "userLogin.html"
         }).catch((error) =>{
             console.log(error);
         })
-        // let userData = JSON.parse(localStorage.getItem("userDetails")) || [];
-        // let obj = {
-        //     name : name,
-        //     email : email,
-        //     password : pass
-        // };
-
-        // userData.push(obj);
-        // localStorage.setItem("userDetails",JSON.stringify(userData));
+        
         
     }
 }   
